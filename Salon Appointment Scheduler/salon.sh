5c16ae1f46a819cd0ebce0217b819a24dc03cbe6
Paritosh pal <paritoshpal7796@gmail.com>
2024-06-06 17:20:48 +0530

    Salon Scheduler DONE

--- Salon Appointment Scheduler/salon.sh
+++ Salon Appointment Scheduler/salon.sh
@@ -5,25 +5,57 @@ echo -e "\n~~~~~ SALON APPOINTMENT SCHEDULER ~~~~~\n"
 
 MAIN_MENU() {
   if [[ $1 ]]
-    then
-      echo -e "\n$1"
+  then
+    echo -e "\n$1"
   fi
 
-  echo "What woud you like?" 
+  echo "What would you like?" 
   # get ALL Services
-  ALL_SERVICES=$($PSQL "SELECT service_id, name FROM services order by service_id")
+  ALL_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
   echo "$ALL_SERVICES" | while read SERVICE_ID BAR NAME 
   do
     echo "$SERVICE_ID) $NAME"
   done
-  read MAIN_MENU_SELECTION
+  read SERVICE_ID_SELECTED
+
+  # Check if the service exists
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  if [[ -z $SERVICE_NAME ]]
+  then
+    MAIN_MENU "Invalid service selection. Please try again."
+  else
+    SCHEDULE_APPOINTMENT 
+  fi
 }
-SCHEDULE_APPOINTMENT () {
 
+SCHEDULE_APPOINTMENT() {
+  echo -e "\nPlease enter your phone number:"
+  read CUSTOMER_PHONE
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # get new customer name
+    echo -e "\nWhat's your name?"
+    read CUSTOMER_NAME
 
+    # insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')") 
+  fi
+
+  # get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  echo -e "\nPlease enter your desired appointment time:"
+  read SERVICE_TIME   
+
+  # insert appointment
+  INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(time, customer_id, service_id) VALUES('$SERVICE_TIME', $CUSTOMER_ID, $SERVICE_ID_SELECTED)")
+
+  # get the service name for the confirmation message
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  
+  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
 }
-
-
 
 MAIN_MENU
-
